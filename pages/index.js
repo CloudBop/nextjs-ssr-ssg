01a7d7bfe1ab511714de7a49c4d51cda583b4258
Cloudbop <boppincolin@gmail.com>
2021-03-15 22:53:38 +0000

    implmntd getStaticProps - SSR mimicing backend database to create SSG

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+import path from "path";
+import fs from "fs/promises";
+
 function HomePage(props) {
+  //
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map(prod => {
+        return <li key={prod.id}>{prod.title}</li>;
+      })}
     </ul>
   );
+}
+
+// this is all ssr - it's never run on client
+// it is run at build time to create 'static' / 'pre-rendered' pages (SSG)
+export async function getStaticProps() {
+  // next understands not to bundle libs/deps used here on client
+
+  // cwd !== /pages
+  // cwd === root dir - process.cwd()
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products
+    }
+  };
 }
 
 export default HomePage;
