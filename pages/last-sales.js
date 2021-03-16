b7cd43d471345c72cf9063b7f45d3e8f6edaa7b8
Cloudbop <boppincolin@gmail.com>
2021-03-16 01:46:29 +0000

    addd custom fetch using useSWR

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,41 +1,40 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
   const [sales, setSales] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
+
+  const { data, error } = useSWR(
+    "https://my-nextjs-db-default-rtdb.firebaseio.com/sales.json"
+  );
 
   useEffect(() => {
-    if (document) {
-      // warning dummy. will break after 30 days
-      fetch("https://my-nextjs-db-default-rtdb.firebaseio.com/sales.json")
-        .then(response => response.json())
-        .then(data => {
-          if (data) {
-            console.log(`data`, data);
-            const intoArray = [];
+    if (data) {
+      const intoArray = [];
 
-            for (const key in data) {
-              // if (Object.hasOwnProperty.call(object, key)) {
-              intoArray.push({
-                id: key,
-                username: data[key].username,
-                volume: data[key].volume
-              });
-              // }
-            }
-
-            setSales(intoArray);
-            setLoading(false);
-          }
+      for (const key in data) {
+        // if (Object.hasOwnProperty.call(object, key)) {
+        intoArray.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume
         });
-    }
-  }, []);
+        // }
+      }
 
-  if (!sales) {
-    return <p>No data yet...</p>;
+      setSales(intoArray);
+    }
+    // return () => {
+    //   cleanup
+    // }
+  }, [data]);
+
+  if (error) {
+    return <p>Failed to load.</p>;
   }
 
-  if (loading) {
+  if (!data || !sales === null) {
     return <p>loading...</p>;
   }
 
@@ -54,3 +53,30 @@ function LastSalesPage() {
 }
 
 export default LastSalesPage;
+
+// useEffect(() => {
+//   if (document) {
+//     // warning dummy. will break after 30 days
+//     fetch("https://my-nextjs-db-default-rtdb.firebaseio.com/sales.json")
+//       .then(response => response.json())
+//       .then(data => {
+//         if (data) {
+//           console.log(`data`, data);
+//           const intoArray = [];
+
+//           for (const key in data) {
+//             // if (Object.hasOwnProperty.call(object, key)) {
+//             intoArray.push({
+//               id: key,
+//               username: data[key].username,
+//               volume: data[key].volume
+//             });
+//             // }
+//           }
+
+//           setSales(intoArray);
+//           setLoading(false);
+//         }
+//       });
+//   }
+// }, []);
